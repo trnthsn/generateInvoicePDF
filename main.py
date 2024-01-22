b5e3385900ff9a619f211c31f935a36054193cb7
Trần Thanh Sơn <son.tt1@teko.vn>
2024-01-22 23:42:14 +0700

    update html invoice

--- main.py
+++ main.py
@@ -72,29 +72,51 @@ def generate_invoice():
     content = generate_invoice_content(ledgers)
 
     html_content = f'''
-        <div>
-            <div style="display: flex; flex-direction: column; margin: 24px; justify-content: space-between">
-                <div>
-                    <div style="display: flex; flex-direction: row", justify-content: space-between">
-                        <div style="display: block; width: 33%" align="left">
-                            <p style="font-weight: bold; margin: 4px 0">{building_name}</p>
-                            <p style="margin: 4px 0">{company_number}</p>
-                            <p style="margin: 4px 0">{address_line_1}</p>
-                            <p style="margin: 4px 0">{address_line_2}</p>
-                        </div>
-                        <div style="display: block; width: 33%" align="center">
-                            <p style="font-weight: bold;">Facturenlijst - grootboekrekening</p>
-                            <p>{date_start} - {date_end}</p>
-                        </div>
-                        <div style="display: block; width: 33%" align="right">
-                            <p style="font-weight: bold;">{export_date}</p>
-                        </div>
+        <div style="margin: auto; width: 595px; height: 842px;">
+        <div style="display: flex; display: -webkit-flex; flex-direction: column; height: 100%; width: 100%; margin: 24px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter; justify-content: space-between; webkit-justify-content: space-between; webkit-box-pack: justify; -ms-flex-pack: justify">
+            <div>
+                <div style="display: -webkit-box; display: -webkit-flex;">
+                    <div style="width: 33%" align="left">
+                        <p style="font-weight: bold; margin: 4px 0">{building_name}</p>
+                        <p style="margin: 4px 0">{company_number}</p>
+                        <p style="margin: 4px 0">{address_line_1}</p>
+                        <p style="margin: 4px 0">{address_line_2}</p>
                     </div>
-                    
+                    <div style="width: 33%" align="center">
+                        <p style="font-weight: bold;">Facturenlijst - grootboekrekening</p>
+                        <p>{date_start} - {date_end}</p>
+                    </div>
+                    <div style="width: 33%" align="right">
+                        <p style="font-weight: bold;">{export_date}</p>
+                    </div>
                 </div>
-                
+                <div>
+                    <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                        <!--column header-->
+                        <tr>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); padding-right: 8px; font-weight: bold;" align="left">Datum</td>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); padding-right: 8px; font-weight: bold;" align="left">Factuurnr.</td>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); padding-right: 8px; font-weight: bold;" align="left">Leverancier.</td>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); padding-right: 8px; font-weight: bold;" align="left">Omschrijving</td>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); width: 72px; padding-right: 8px; font-weight: bold;" align="right">BTW</td>
+                            <td style="border-bottom: 1px solid rgba(0, 0, 0, 0.10); width: 72px; padding-left: 4px; font-weight: bold;" align="right">Total</td>
+                        </tr>
+                        {content}
+                    </table>
+                </div>
+            </div>
+            <div>
+                <table style="width: 100%; margin-top: 16px; border: 0; border-spacing: 0px 8px; line-height: 16px; word-wrap: break-word; font-size: 11px; font-family: Inter;">
+                    <!--footer-->
+                    <tr>
+                        <td style="border-top: 1px solid rgba(0, 0, 0, 0.10); padding-right: 8px; font-weight: bold;" align="left">Total</td>
+                        <td style="border-top: 1px solid rgba(0, 0, 0, 0.10); width: 72px; padding-right: 8px; font-weight: bold;" align="right">&euro;{sum_vat_amount}</td>
+                        <td style="border-top: 1px solid rgba(0, 0, 0, 0.10); width: 72px; padding-left: 4px; font-weight: bold;" align="right">&euro;{sum_total_amount}</td>
+                    </tr>
+                </table>
             </div>
         </div>
+    </div>
         '''
 
     # html_file = open("result.html", "w")
